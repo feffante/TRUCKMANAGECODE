0c5e4a1a5ee39a541c5103f35820110fe289923d
feffante <federico.casali@gmail.com>
2021-01-13 09:31:35 +0100

    inserito new mvc

---
+++ TRUCKMANAGEMENT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TRUCKMANAGEMENT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
