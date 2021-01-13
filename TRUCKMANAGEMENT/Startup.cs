cfafe27aa829c5f59db7d07610ceb28df5bc4d37
feffante <federico.casali@gmail.com>
2021-01-13 20:11:41 +0100

    inserito il logo am non si vede

--- TRUCKMANAGEMENT/Startup.cs
+++ TRUCKMANAGEMENT/Startup.cs
@@ -37,15 +37,17 @@ namespace TRUCKMANAGEMENT
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+               // app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
+           
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+ app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
